7074be1411bd9f341ed102483b0fa50e17ac9d61
nunes <amsjnunes@live.com.pt>
2018-11-30 12:28:38 +0100

    code complet et functionnel

--- index.php
+++ index.php
@@ -11,12 +11,16 @@
 		$json_url = "data.txt";
 		$json = file_get_contents($json_url);
 		$data = json_decode($json);
+
+		$baragraph_height = 161 + $data->temperature * 4;
+		$baragraph_top = 315 - $data->temperature * 4;
 	?>
 
 	<p> il fait <?php echo $data->temperature; ?> °C avec <?php echo $data->humidite; ?>% d'humidité. Le 30/11/2018</p>
 
 	<div id="thermometer">
-	<div id="bargraph"></div>
+		<div id="baragraph" style="height:<?php echo $baragraph_height; ?>px; top:<?php echo $baragraph_top; ?>px;"></div>
+	</div>
 
 </body>
 </html>
