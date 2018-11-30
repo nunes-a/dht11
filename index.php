27d041454dcd43471e67e2028552bb3adbf0c408
nunes <amsjnunes@live.com.pt>
2018-11-30 11:43:14 +0100

    début code php

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 	<h1>Température</h1>
 
 	<?php
-		
+		$json_url = data.txt;
+		$json = file_get_contents(json_url);
+		$data = json_decode($json, TRUE);
+		print_r($data);
 	?>
 	<p>Il fait </p>
 
