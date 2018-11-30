7f5743f863c854b05f69825a1e8ca4254341959b
nunes <amsjnunes@live.com.pt>
2018-11-30 11:48:27 +0100

    code partager pour fonctioner le temp et humid

--- index.php
+++ index.php
@@ -7,13 +7,15 @@
 	<h1>Température</h1>
 
 	<?php
-		$json_url = data.txt;
-		$json = file_get_contents(json_url);
-		$data = json_decode($json, TRUE);
-		print_r($data);
+		$json_url = "data.txt";
+		$json = file_get_contents($json_url);
+		$data = json_decode($json);
 	?>
-	<p>Il fait </p>
 
-	<img src="imgs/thermometer.jpg">
+	<p> il fait <?php echo $data->temperature; ?> °C avec <?php echo $data->humidite; ?>% d'humidité. Le 30/11/2018</p>
+
+	<div id="thermometer">
+	<div id="bargraph"></div>
+	
 </body>
 </html>
