3fc6dd4dfa8927c4b10dfca39c0aa8588edd7143
nunes <amsjnunes@live.com.pt>
2018-11-30 11:38:12 +0100

    Nouveau fichier .php (aprés me tromper)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Température</h1>
+
+	<?php
+		
+	?>
+	<p>Il fait </p>
+
+	<img src="imgs/thermometer.jpg">
+</body>
+</html>
