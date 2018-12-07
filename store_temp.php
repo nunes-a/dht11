372657afcf5d110d0f14a86a1796af1125197bc8
nunes <amsjnunes@live.com.pt>
2018-12-07 10:09:24 +0100

    affichage de la temperature d'input

--- store_temp.php
+++ store_temp.php
@@ -1,3 +1,15 @@
 <?php
-echo
+if(isset($_POST['temperature'])){
+	file_put_contents("temperature.txt", $_POST['temperature']);
+}
+
+
+/*
+$fichier = fopen('temperature.txt', 'w+');
+$texte = $_POST["temperature"];
+fwrite ($fichier, $texte);
+fclose ($fichier);
+*/
+
+echo $_POST["temperature"];
 ?>
