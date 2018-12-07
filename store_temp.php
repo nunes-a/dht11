abc89f1f771b3b0fccaf68f3399f09bb31397cb7
nunes <amsjnunes@live.com.pt>
2018-12-07 10:26:35 +0100

    nv etape

--- store_temp.php
+++ store_temp.php
@@ -1,15 +1,12 @@
 <?php
 if(isset($_POST['temperature'])){
-	file_put_contents("temperature.txt", $_POST['temperature']);
+	/*test pour garantir */
+
+	if (!file_put_contents("temperature.txt", $_POST['temperature']))
+	/*ce code va ecrire le valeur que nous avons ecrit dans le input avec le name "temperature" dans le fichier "temperature.txt"*/
+		echo "erreur";
 }
+else
+	echo "erreur";
 
-
-/*
-$fichier = fopen('temperature.txt', 'w+');
-$texte = $_POST["temperature"];
-fwrite ($fichier, $texte);
-fclose ($fichier);
-*/
-
-echo $_POST["temperature"];
 ?>
